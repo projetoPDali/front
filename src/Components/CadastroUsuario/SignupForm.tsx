8a3c965b89693fdfa992b8cd37d30fa8fb212cdb
Beatriz R. Montanini <montanini249@gmail.com>
2023-10-26 17:30:45 -0300

    estilo botão google

--- src/Components/CadastroUsuario/SignupForm.tsx
+++ src/Components/CadastroUsuario/SignupForm.tsx
@@ -4,7 +4,8 @@ import { cadastrarUser } from "../../api/cadastrarUser";
 import { inputStyle } from "./styles";
 import google from "../../assets/google.png";
 import YellowButton from "../Buttons/YellowBotton";
-import GoogleLogin from 'react-google-login';
+import GoogleLogin from "react-google-login";
+import COLORS from "../../constant/colors";
 
 const SignupForm: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
@@ -67,19 +68,39 @@ const SignupForm: React.FC = () => {
         )}
 
         <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
-          <Form.Control type="text" placeholder="Email" name="mail" style={inputStyle} />
+          <Form.Control
+            type="text"
+            placeholder="Email"
+            name="mail"
+            style={inputStyle}
+          />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="name">
-          <Form.Control type="text" placeholder="Nome" name="name" style={inputStyle} />
+          <Form.Control
+            type="text"
+            placeholder="Nome"
+            name="name"
+            style={inputStyle}
+          />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="alias">
-          <Form.Control type="text" placeholder="Nome de Usuário" name="alias" style={inputStyle} />
+          <Form.Control
+            type="text"
+            placeholder="Nome de Usuário"
+            name="alias"
+            style={inputStyle}
+          />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="phone">
-          <Form.Control type="text" placeholder="Telefone" name="phone" style={inputStyle} />
+          <Form.Control
+            type="text"
+            placeholder="Telefone"
+            name="phone"
+            style={inputStyle}
+          />
         </Form.Group>
 
         <div
@@ -92,21 +113,32 @@ const SignupForm: React.FC = () => {
         >
           <YellowButton type="submit" content="Cadastrar" />
           <GoogleLogin
-  clientId="977680160090-qgos9d3eqrsu68ptnbl5q096cq8pvopj.apps.googleusercontent.com"
-  buttonText="Continuar com o Google"
-  onSuccess={responseGoogle}
-  onFailure={responseGoogle}
-  cookiePolicy={'single_host_origin'}
-  render={renderProps => (
-    <button
-      onClick={renderProps.onClick}
-      disabled={renderProps.disabled}
-      className="YellowButton" // Adicione a classe de estilo que você deseja
-    >
-      Personalizado
-    </button>
-  )}
-/>
+            clientId="977680160090-qgos9d3eqrsu68ptnbl5q096cq8pvopj.apps.googleusercontent.com"
+            buttonText="Continuar com o Google"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+            render={(renderProps) => (
+              <button
+                style={{
+                  backgroundColor: COLORS.secondary,
+                  borderColor: COLORS.secondary,
+                  borderWidth: 0,
+                  fontWeight: "bold",
+                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)",
+                  minWidth: "151px", // Defina a largura mínima desejada
+                  minHeight: "41px",
+                  color: COLORS.white,
+                  borderRadius: 7,
+                }}
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="YellowButton" // Adicione a classe de estilo que você deseja
+              >
+                <img src={google} alt="logo google" width={30} height={30}/>
+              </button>
+            )}
+          />
         </div>
       </Form>
     </div>
