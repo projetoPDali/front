b94de94e8c0fa1253f52100e991fa337a1b00b6f
Beatriz R. Montanini <montanini249@gmail.com>
2023-11-19 19:31:49 -0300

    enviado filtros para home

--- src/Components/Home/Search.tsx
+++ src/Components/Home/Search.tsx
@@ -1,26 +1,59 @@
+import React, { useState } from "react";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import { Col, Row } from "react-bootstrap";
 import COLORS from "../../constant/colors";
 
-const buttonsData = [
+interface Filter {
+  label: string;
+  actions: string[];
+}
+
+const buttonsData: Filter[] = [
   { label: "Material", actions: ["Fibra de Carbono", "Alumínio", "Aço"] },
-
   { label: "Marca", actions: ["Sense", "Caloi", "Oggi", "Trek", "Giant"] },
-
   { label: "Marcha", actions: ["Sim", "Não"] },
   { label: "Quadro", actions: ["Pequeno", "Médio", "Grande", "Extra Grande"] },
   { label: "Aro", actions: [] },
   { label: "Valor Hora", actions: ["Menos de R$50", "Entre R$50 e R$70"] },
   { label: "Valor Dia", actions: ["Menos de R$100", "Entre R$110 e R$150"] },
-  { label: "Suspenção", actions: ["sim", "não"] },
+  { label: "Suspensão", actions: ["sim", "não"] },
   { label: "Genero", actions: ["Feminino", "Masculino", "Unissex"] },
-
-
+  { label: "Limpar Filtros", actions: [] },
 ];
 
-const Search = () => {
+const Search = ({ onFilterChange }: { onFilterChange: (newSelectedFilters: string[]) => void }) => {
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  const handleFilterChange = (filterLabel: string, action: string) => {
+    const newSelectedFilters: string[] = [...selectedFilters];
+    const filterString = `${filterLabel}-${action}`;
+
+    // Check if the filter is already selected
+    const filterIndex = newSelectedFilters.indexOf(filterString);
+
+    if (filterIndex !== -1) {
+      // If selected, remove it
+      newSelectedFilters.splice(filterIndex, 1);
+    } else {
+      // If not selected, add it
+      newSelectedFilters.push(filterString);
+    }
+
+    setSelectedFilters(newSelectedFilters);
+    onFilterChange(newSelectedFilters);
+  };
+
+  const clearAllFilters = () => {
+    setSelectedFilters([]);
+    onFilterChange([]);
+  };
+
+  const isFilterSelected = (filterLabel: string, action: string) => {
+    return selectedFilters.includes(`${filterLabel}-${action}`);
+  };
+
   return (
     <Row style={{ display: "flex", justifyContent: "center" }}>
       {buttonsData.map((button, index) => (
@@ -32,24 +65,32 @@ const Search = () => {
             alignContent: "space-around",
             backgroundColor: COLORS.primary,
           }}
+          key={index}
         >
           <DropdownButton
             style={{ color: COLORS.white }}
             as={ButtonGroup}
-            key={index}
             id={`dropdown-variants-${button.label}`}
             variant={button.label.toLowerCase()}
             title={button.label}
           >
             {button.actions.map((action, actionIndex) => (
               <Dropdown.Item
-                style={{ backgroundColor: COLORS.white }}
+                style={{
+                  backgroundColor: isFilterSelected(button.label, action) ? COLORS.lightGreen : COLORS.white,
+                }}
                 key={actionIndex}
-                eventKey={actionIndex + 1}
+                onClick={() => handleFilterChange(button.label, action)}
               >
                 {action}
               </Dropdown.Item>
             ))}
+            {index === buttonsData.length - 1 && (
+              <>
+                
+                <Dropdown.Item onClick={clearAllFilters}>Limpar Filtros</Dropdown.Item>
+              </>
+            )}
           </DropdownButton>
         </Col>
       ))}
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MainNavbar from "../Components/Navbar/Navbar";
 import Header from "../Components/Home/Header";
 import CardBike from "../Components/Home/CardBike";
 import Search from "../Components/Home/Search";
-import { useAuth } from "../Context/AuthContext"; // Import useAuth
+import { useAuth } from "../Context/AuthContext";
 import { Container } from "react-bootstrap";
 
 const Home = () => {
   const { user } = useAuth();
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  const handleFilterChange = (newSelectedFilters: string[]) => {
+    setSelectedFilters(newSelectedFilters);
+    console.log("Selected Filters in Home:", newSelectedFilters);
+  };
 
   return (
     <div>
       <MainNavbar />
       <Header />
-      <Container fluid><Search /></Container>
+      <Container fluid>
+        <Search onFilterChange={handleFilterChange} />
+      </Container>
       <CardBike />
-
-     
     </div>
   );
 };
